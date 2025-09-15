d4e65024f46118c771522b547518684cae958a05
Joel Doctor <joeldoctor47@gmail.com>
2025-09-15 14:42:53 +0800

    set-up drizzle, docker, initial database

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,4 @@
+export * from "./schema/user"
+export * from "./schema/jobinfo"
+export * from "./schema/interview"
+export * from "./schema/question"
---
+++ src/drizzle/schema/interview.ts
@@ -0,0 +1,21 @@
+import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "../schemaHelpers";
+import { JobInfoTable } from "./jobinfo";
+import { relations } from "drizzle-orm";
+
+export const InterviewTable = pgTable("interviews", {
+    id,
+    jobInfoId: uuid().references(() => JobInfoTable.id, { onDelete: "cascade" }).notNull(),
+    duration: varchar().notNull(),
+    humeChatId: varchar(),
+    feedback: varchar(),
+    createdAt,
+    updatedAt
+})
+
+export const interviewRelations = relations(InterviewTable, ({ one }) => ({
+    jobInfo: one(JobInfoTable, {
+        fields: [InterviewTable.jobInfoId],
+        references: [JobInfoTable.id]
+    })
+}));
