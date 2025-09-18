15cf408bd619ceaac815654a95f0acb6d886bb85
Joel Doctor <joeldoctor47@gmail.com>
2025-09-18 13:02:26 +0800

    added redirection based on authentication

---
+++ src/app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function AppPage() {
+  return (
+    <main className='container flex flex-col items-center justify-center h-screen gap-4'>
+      <h1 className='text-4xl'>Welcome to Next.js!</h1>
+      <p className='text-lg'>Get started by editing app/page.tsx</p>
+    </main>
+  );
+}
